72d88a98f202a1fab5d0a33f4dafda3ae7065146
sergiotvidal <sergiotvidal2@gmail.com>
2019-06-20 17:36:47 +0200

    deploy changes

--- src/app/features/search/map/map.component.ts
+++ src/app/features/search/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../../../core/services/search.service';
+import { Local } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-map',
